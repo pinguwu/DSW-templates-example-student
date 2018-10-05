82b41ae84191025755692c381ddd4820e0c195a5
pinguwu <42652753+pinguwu@users.noreply.github.com>
2018-10-05 10:50:33 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -13,6 +13,10 @@ def render_page1():
 @app.route("/p2")
 def render_page2():
     return render_template('page2.html')
+
+@app.route("/p3")
+def render_page2():
+    return render_template('page3.html')
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
